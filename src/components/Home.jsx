46648ec781f37a0b153b3b73df7b79c3ef27673d
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2022-12-28 18:42:22 +0530

    commited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
                 <div className="flex flex-col justify-center h-full">
                     <h2 className="text-4xl sm:text-5xl text-white font-bold">I'm a Full Stack Developer</h2>
-                    <p className="text-gray-400 py-4 ">
+                    <p className="text-gray-400 py-5 ">
                         A paragraph (from Ancient Greek παράγραφος (parágraphos) 'to write beside') is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose.
                     </p>
                     <div>
@@ -21,7 +21,7 @@ const Home = () => {
                     </div>
                 </div>
                 <div>
-                    <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-50"></img>
+                    <img src={HeroImage} alt="my profile " className="rounded-2xl  mx-auto w-2/3 md:w-2/4"></img>
                 </div>
             </div>
         </div>
